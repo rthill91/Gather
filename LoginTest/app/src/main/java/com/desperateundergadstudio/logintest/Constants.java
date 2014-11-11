7b7220888f1a9f604e4dea8106d9077896473240
Ryan Hill <rthill91@gmail.com>
2014-11-10 18:15:53 -0600

    Added loginTest project

---
+++ LoginTest/app/src/main/java/com/desperateundergadstudio/logintest/Constants.java
@@ -0,0 +1,10 @@
+package com.desperateundergadstudio.logintest;
+
+/**
+ * Created by Ryan on 11/9/2014.
+ */
+public class Constants {
+    static String api_base = "http://rthill91.synology.me:8081";
+    static String login = "/user/login";
+    static String register = "/user/register";
+}
