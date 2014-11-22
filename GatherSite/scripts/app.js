98e18c1cef2b7b720d202bc7d2f454664c4e95de
Ryan Hill <rthill91@gmail.com>
2014-11-22 09:25:47 -0600

    add GET test to site

--- GatherSite/scripts/app.js
+++ GatherSite/scripts/app.js
@@ -4,20 +4,27 @@ app.controller('mainController', function($scope) {
 
 });
 
-app.controller('registerController', function($scope, $log) {
+app.controller('registerController', function($scope, $http, $log) {
 	$scope.user = {};
 
 	function validateForm() {
 		if(($scope.user.username) &&
 		($scope.user.password) &&
 		($scope.user.password === $scope.confirmPassword)) {
+			
 			return true;
 		} else return false;
 	}
 
 	$scope.submitForm = function() {
 		if(validateForm()) {
-			$log.debug("Send Register To API");
+		$http.get("http://rthill91.synology.me:8081")
+		.success(function(data, status, headers, config) {
+			$log.log(data);
+		})
+		.error(function(data, status, headers, config) {
+			$log.error(data);
+		});
 		} else {
 			$log.debug("Invalid Credentials");
 		}
