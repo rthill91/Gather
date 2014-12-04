4ad5f6c30c1b26beb165864c7df72c3d875349c2
Noah Williams <noahwilliams42@gmail.com>
2014-12-04 12:39:28 -0600

    Created functionality on choose event location page, changed string for title, added new dimensions so no padding on map

--- Gather/app/src/main/java/com/desperateundergadstudio/gather/LocationNewEventActivity.java
+++ Gather/app/src/main/java/com/desperateundergadstudio/gather/LocationNewEventActivity.java
@@ -1,15 +1,29 @@
 package com.desperateundergadstudio.gather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class LocationNewEventActivity extends Activity {
+public class LocationNewEventActivity extends Activity implements OnMapClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +31,39 @@ public class LocationNewEventActivity extends Activity {
         setContentView(R.layout.activity_location_new_event);
 
         GoogleMap MapFrag = ((MapFragment) getFragmentManager().findFragmentById(R.id.createEvent_map)).getMap();
-        MapFrag.setOnMapClickListener((GoogleMap.OnMapClickListener) this);
+        MapFrag.setOnMapClickListener((OnMapClickListener) this);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        Button button = new Button(this);
+        button.setText("Submit");
+        addContentView(button, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        button.setX(button.getX() + 50f);
+        button.setY(size.y/2 + 400f);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                LocationNewEventActivity.this.finish();
+            }
+        });
     }
 
+    @Override
+    public void onMapClick(LatLng point) {
+        GoogleMap MapFrag = ((MapFragment) getFragmentManager().findFragmentById(R.id.createEvent_map)).getMap();
+            MapFrag.addMarker(new MarkerOptions()
+            .position(point)
+            .title("New Event"));
+        SharedPreferences prefs = getSharedPreferences(Constants.session_prefs,0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putFloat("NewEventLatitude", (float)point.latitude);
+        editor.putFloat("NewEventLongitude", (float)point.longitude);
+        editor.apply();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
